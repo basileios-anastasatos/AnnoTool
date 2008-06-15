6cea959dd1bc033c9f8ed0f95b0e5c0e19b651cb
Oliver Schwahn <olli.schwahn@gmx.net>
2008-06-15 19:09:22 +0000

    renaming
    git-svn-id: https://svn.d2.mpi-inf.mpg.de/D2/schwahn@75 841fb669-bbcc-474c-9fc9-e830e8f3ae44

---
+++ AnnoTool/src/uiStuff/ZoomControl.cpp
@@ -0,0 +1,50 @@
+#include "include/ZoomControl.h"
+#include <QLineEdit>
+
+ZoomControl::ZoomControl(QWidget *parent) :
+    QWidget(parent) {
+    ui.setupUi(this);
+    initZoomList();
+}
+
+ZoomControl::~ZoomControl() {
+}
+
+void ZoomControl::initZoomList() {
+    ui.cbZoom->addItem("10");
+    ui.cbZoom->addItem("25");
+    ui.cbZoom->addItem("50");
+    ui.cbZoom->addItem("75");
+    ui.cbZoom->addItem("100");
+    ui.cbZoom->addItem("150");
+    ui.cbZoom->addItem("200");
+    ui.cbZoom->addItem("300");
+    ui.cbZoom->addItem("400");
+    ui.cbZoom->addItem("500");
+}
+
+void ZoomControl::on_zoomSlider_valueChanged(int value) {
+    ui.cbZoom->lineEdit()->setText(QString("%1").arg(value));
+    emit zoomChanged(value);
+}
+
+void ZoomControl::on_cbZoom_editTextChanged(const QString &text) {
+    bool isOk = false;
+    int value = text.toInt(&isOk, 10);
+    if (isOk && value >= 10 && value <= 500) {
+        ui.zoomSlider->setValue(value);
+    } else if (!isOk) {
+        on_zoomSlider_valueChanged(ui.zoomSlider->value());
+    }
+}
+
+void ZoomControl::setZoom(int value) {
+    ui.zoomSlider->setValue(value);
+}
+
+int ZoomControl::getZoom() {
+    return ui.zoomSlider->value();
+}
+
+
+// vim:ts=4:sts=4:sw=4:tw=80:expandtab
